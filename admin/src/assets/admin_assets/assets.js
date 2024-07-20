5a7316374cd9a7af525cbfdf97591b23ceb5595d
Aman asati <106515544+createwithaman@users.noreply.github.com>
2024-07-20 13:03:02 +0530

    Update assets.js

--- admin/src/assets/admin_assets/assets.js
+++ admin/src/assets/admin_assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+
+
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +16,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-app-6jmd.onrender.com'
